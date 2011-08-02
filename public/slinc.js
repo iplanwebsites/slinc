b82a9248707c7a04ddb8a55bb417e3f09a6e32ec
I Plan Websites <info@iplanwebsites.com>
2011-08-02 13:39:19 -0400

    typo

--- public/slinc.js
+++ public/slinc.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 
 //redirection.
 if(window.location.hostname == 'www.slincom.ca'){
-	window.location = "http://slincom.com/";
+	window.location = "http://slincom.ca/";
 }
 
 
